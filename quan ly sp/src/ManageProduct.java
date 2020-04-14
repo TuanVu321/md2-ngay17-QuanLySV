a9c03675786d7b28562c1c2be5f1a34228b37fb1
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-04-14 08:39:11 +0700

    Add files via upload

--- quan ly sp/src/ManageProduct.java
+++ quan ly sp/src/ManageProduct.java
@@ -3,23 +3,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ManageProduct {
-    public static void creatArr(){
+    public static void creatArr() {
 
     }
 
 
-    public static void writeFile(String path, List<Product> arr, int code, String name, String made, double price, String desc ) {
+    public static void writeFile(String path, List<Product> arr, int code, String name, String made, double price, String desc) {
 
         FileOutputStream fileOutputStream = null;
         ObjectOutputStream objectOutputStream = null;
         try {
             fileOutputStream = new FileOutputStream(path);
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            Product product = new Product(code, name,made,price,desc);
+            Product product = new Product(code, name, made, price, desc);
             arr.add(product);
-            for(Product pr:arr){
-                objectOutputStream.writeObject(pr);
-            }
+            arrToFile(arr, objectOutputStream);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -27,6 +25,12 @@ public class ManageProduct {
 //            e.printStackTrace();
         }
 
+    }
+
+    private static void arrToFile(List<Product> arr, ObjectOutputStream objectOutputStream) throws IOException {
+        for (Product pr : arr) {
+            objectOutputStream.writeObject(pr);
+        }
     }
 
     public static void readFile(String path) {
@@ -59,17 +63,42 @@ public class ManageProduct {
 
     }
 
-    public static void findCode(String path,int code){
-        List<Product> findByCode = fileToArr(path);
-        for (Product product : findByCode) {
+    public static void findCode(String path, int code) {
+        List<Product> arr = fileToArr(path);
+        for (Product product : arr) {
             if (product.getCode() == code) {
-                System.out.println("ket qua tim duoc la: "+product);
+                System.out.println("ket qua tim duoc la: " + product);
             }
         }
     }
 
+    public static void delete(String path, int code) {
+        List<Product> arr = fileToArr(path);
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i).getCode() == code) {
+                arr.remove(i);
+            }
+        }
+        arrToFile("danh sach.txt", arr);
+    }
+
+    public static void change(String path, int code, int codeFix, String name, String made, double price, String desc) {
+        List<Product> arr = fileToArr(path);
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i).getCode() == code) {
+               arr.get(i).setCode(codeFix);
+                arr.get(i).setName(name);
+                arr.get(i).setMade(made);
+                arr.get(i).setPrice(price);
+                arr.get(i).setDesc(desc);
+            }
+        }
+        arrToFile("danh sach.txt", arr);
+    }
+
+
     public static List<Product> fileToArr(String path) {
-        List <Product> findByCode = new ArrayList<>();
+        List<Product> findByCode = new ArrayList<>();
         FileInputStream fileInputStream = null;
         ObjectInputStream objectInputStream = null;
 
@@ -104,7 +133,7 @@ public class ManageProduct {
         try {
             fileOutputStream = new FileOutputStream(path);
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            for(Product pr:findByCode){
+            for (Product pr : findByCode) {
                 objectOutputStream.writeObject(pr);
             }
 
@@ -118,15 +147,17 @@ public class ManageProduct {
 
     public static void main(String[] args) {
         ManageProduct manageProduct = new ManageProduct();
-        List <Product> products = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
         Product product = new Product(01, "Book1", "China", 1000, "sach giao khoa");
         Product product1 = new Product(02, "Book2", "Taiwan", 2000, "truyen");
         products.add(product);
         products.add(product1);
-        manageProduct.writeFile("danh sach.txt",products,03,"Book3", "japan", 3000, "truyen");
+        manageProduct.writeFile("danh sach.txt", products, 03, "Book3", "japan", 3000, "truyen");
         manageProduct.readFile("danh sach.txt");
-        manageProduct.findCode("danh sach.txt",02);
-
-
+        manageProduct.findCode("danh sach.txt", 02);
+        manageProduct.delete("danh sach.txt", 02);
+        manageProduct.readFile("danh sach.txt");
+        manageProduct.change("danh sach.txt", 03, 04, "Book4", "apan", 4000, "tryen");
+        manageProduct.readFile("danh sach.txt");
     }
 }
